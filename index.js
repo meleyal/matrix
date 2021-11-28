a15c4d3bb2a00cc529cd8e52d4354a344ff45986
William Meleyal <william.meleyal@gmail.com>
2021-11-28 21:59:27 +0000

    worker

--- index.js
+++ index.js
@@ -1,59 +1,97 @@
-import { random } from './lib.js'
-import _ from 'https://unpkg.com/lodash-es'
-
-const channel = (index) => 144 + index
-
-const transpose = (pattern, n = 12) =>
-  pattern.map(([note, velocity]) => [note + n, velocity])
+import { zip, range, random, transpose, channel, useMidi, _ } from './lib.js'
 
 const initialModel = {
-  interval: 250,
-  tick: -1,
-  tracks: [],
-  patterns: [],
+    interval: 100,
+    tick: -1,
+    tracks: [
+        {
+            pattern: [],
+            step: -1,
+            resolution: 1,
+        },
+        {
+            pattern: [],
+            step: -1,
+            resolution: 1,
+        },
+        {
+            pattern: [],
+            step: -1,
+            resolution: 1,
+        },
+    ],
 }
 
-const generate = () => {
-  //   const notes = [60, 62, 64, 65, 67, 69, 71, 72]
-  //   const notes = _.range(8).map((i) => _.random(21, 128))
-  const notes = _.range(8).map((i) => random(60, 72))
-  //   const velocities = [127, 127, 127, 127, 127, 127, 127, 127]
-  const velocities = _.range(8).map((i) => random(60, 127))
-  const pattern = _.zip(notes, velocities)
-  return pattern
+const generate = (length) => {
+    //   const notes = _.range(8).map((i) => _.random(21, 128))
+    const notes = range(length).map(() => random(60, 72))
+    const velocities = range(length).map(() => random(60, 127))
+    const pattern = zip(notes, velocities)
+    return pattern
 }
 
 const update = (msg, model) => {
-  switch (msg) {
-    case 'init':
-      const pattern = generate()
-      model.tracks[0] = pattern
-      model.tracks[1] = transpose(pattern)
-      break
-    case 'tick':
-      model.tick += 1
-      break
-  }
-  console.log(model)
-  return model
+    switch (msg) {
+        case 'init':
+            const pattern = generate(8)
+            model.tracks[0].pattern = pattern
+            model.tracks[1].pattern = transpose(pattern)
+            model.tracks[2].pattern = transpose(pattern, -12)
+            break
+        case 'tick':
+            if (model.tick % 2 === 0) {
+                // console.log('beat')
+                model.tracks = model.tracks.map((track) => {
+                    const step = (track.step + 1) % track.pattern.length
+                    return {
+                        ...track,
+                        step,
+                    }
+                })
+            }
+            model.tick += 1
+            break
+    }
+    // console.log(model)
+    return model
 }
 
-const render = (output, model) => {
-  const { tick, tracks } = model
-  tracks.forEach((track, index) => {
-    const [note, velocity] = track[tick % track.length]
-    output.send([channel(index), note, velocity])
-  })
+let prevModel = initialModel
+const render = (midi, model) => {
+    const { tick, tracks } = model
+
+    // console.log(model.tracks[0].step, prevModel.tracks[0].step)
+    // console.log(model.tracks[0].step === prevModel.tracks[0].step)
+
+    // TODO: skip rendering if step has not changed
+    tracks.forEach((track, index) => {
+        // console.log(prevModel.tracks[index].step, track.step)
+        if (track.step !== prevModel.tracks[index].step) {
+            // console.log(track.step)
+            // console.log(track.pattern[track.step])
+            // const [note, velocity] = track.pattern[tick % track.pattern.length]
+            const [note, velocity] = track.pattern[track.step]
+            midi.send([channel(index), note, velocity])
+        }
+    })
+    // prevStep = model.tracks[0].step
+    prevModel = { ...model }
 }
 
 const main = async () => {
-  const midi = await navigator.requestMIDIAccess()
-  const output = midi.outputs.values().next().value
-  const model = update('init', initialModel)
-  //   setInterval(() => {
-  //     const updatedModel = update('tick', model)
-  //     render(output, updatedModel)
-  //   }, model.interval)
+    const midi = await useMidi()
+    const model = update('init', initialModel)
+    // setInterval(() => {
+    //     const updatedModel = update('tick', model)
+    //     render(midi, updatedModel)
+    // }, model.interval)
+
+    const worker = new Worker('./worker.js')
+    worker.onmessage = (e) => {
+        // console.log('got message', e.data)
+        const updatedModel = update('tick', model)
+        render(midi, updatedModel)
+    }
 }
 
 main()
--- lib.js
+++ lib.js
@@ -1,21 +1,35 @@
+import lodash from 'https://unpkg.com/lodash-es'
 import { prng_alea } from 'https://unpkg.com/esm-seedrandom'
 
 const rng = prng_alea(null, {
-  global: true,
-  state: true,
-  state: {
-    c: 1,
-    s0: 0.8634394553955644,
-    s1: 0.31969695654697716,
-    s2: 0.6573936773929745,
-  },
+    global: true,
+    state: true,
+    // state: {
+    //     c: 1,
+    //     s0: 0.40706299943849444,
+    //     s1: 0.19002798269502819,
+    //     s2: 0.31966522987931967,
+    // },
 })
 const savedState = rng.state()
-console.log(rng())
 console.log(savedState)
 
 export const random = (min, max) => {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(rng() * (max - min + 1) + min) //The maximum is inclusive and the minimum is inclusive
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(rng() * (max - min + 1) + min)
 }
+
+export const channel = (index) => 144 + index
+
+export const transpose = (pattern, n = 12) =>
+    pattern.map(([note, velocity]) => [note + n, velocity])
+
+export const useMidi = async () => {
+    const midi = await navigator.requestMIDIAccess()
+    return midi.outputs.values().next().value
+}
+
+export const zip = lodash.zip
+export const range = lodash.range
+export const _ = lodash
